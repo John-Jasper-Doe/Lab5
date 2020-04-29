a22c2ccb987e255c1c2e7bb78c9724828d24a966
John Doe <john.jasper.doe@gmail.com>
2020-04-29 17:18:06 +0300

    Added tags

---
+++ src/common/tags.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file tags.hpp
+ * @brief The tools discription
+ */
+
+
+#ifndef TAGS_HPP_
+#define TAGS_HPP_
+
+
+namespace common {
+  
+  enum class color_tag : char {
+      red
+    , blue
+    , green    
+    , yellow
+    , white
+    , black
+  };
+
+  enum class tool_tag : char {
+      dot
+    , line
+    , rect
+    , circle
+    , eraser
+  };
+
+  using color_tag_t = common::color_tag;
+  using tool_tag_t = common::tool_tag;
+
+} /* common:: */
+
+
+#endif /* TAGS_HPP_ */

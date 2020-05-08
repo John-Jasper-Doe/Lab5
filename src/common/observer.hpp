abd14c105d55e39989b350d0450a34faf8106779
John Doe <john.jasper.doe@gmail.com>
2020-05-08 22:02:45 +0300

    Added "observer" template

---
+++ src/common/observer.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file observer.hpp
+ * @brief Description of the observer template class.
+ */
+
+
+#include <list>
+#include <functional>
+
+namespace common {
+
+template <class ...ARGS>
+using func_ptr_t = std::function<void(const ARGS& ...)>;
+
+template <class ...ARGS>
+class observer {
+  std::list<func_ptr_t<ARGS...>> listeners_;
+
+public:
+  virtual ~observer<ARGS...>() = default;
+
+  void add_listener(func_ptr_t<ARGS...> listener) {
+    listeners_.push_back(std::move(listener));
+  }
+
+  void notify_update(const ARGS& ...args) {
+    for (auto& listener: listeners_)
+      listener(args...);
+  }
+};
+
+} /* common:: */
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,37 @@
  */
 
 
+
+#include "common/observer.hpp"
+
+
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+
+using namespace std::chrono_literals;
+void print_main1() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+void print_main2(const int a) { std::cout << __PRETTY_FUNCTION__ << ": " << a << std::endl; }
 
 
 int main() {
   std::cout << "Editor" << std::endl;
 
-  return 0
+  common::observer<> l1;
+  common::observer<int> l2;
+
+  l1.add_listener(print_main1);
+
+//  auto l = [](int s) { print_main2(s); };
+  l2.add_listener(print_main2);
+
+  std::this_thread::sleep_for(3s);
+
+  l1.notify_update();
+  l2.notify_update(5);
+
+  return 0;
 }
 
